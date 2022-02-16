36889df3dac9f93bd5a19113eaac91989cc0d239
RobertWeng <99759244+RobertWeng@users.noreply.github.com>
2022-02-16 09:14:13 +0800

    Update StudentRepository.java
    - Fix query bug

--- src/main/java/com/example/school/dao/StudentRepository.java
+++ src/main/java/com/example/school/dao/StudentRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    @Query("SELECT s FROM Student s JOIN StudentSchedule o on o.student = s JOIN Schedule z ON z = o.schedule AND NOT EXISTS(SELECT 1 FROM StudentSchedule WHERE student = s AND schedule = :schedule)")
+    @Query("SELECT s FROM Student s WHERE NOT EXISTS(SELECT 1 FROM StudentSchedule WHERE student = s AND schedule = :schedule)")
     List<Student> findAllBySchedule(@Param("schedule") Schedule schedule);
 }
